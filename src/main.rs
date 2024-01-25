0a6904a377da2efd748811ffdb79b74c6af32c9a
J4spr <jasper7.verbruggen@gmail.com>
2024-01-25 17:36:03 +0100

    implemnted breakpoints and cpu exceptions

--- src/main.rs
+++ src/main.rs
@@ -1,82 +1,47 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
-#![test_runner(self::test_runner)]
+#![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use blog_os::println;
 use core::panic::PanicInfo;
-use blog_os::println as blog_os_println; // Rename the conflicting import
-
-mod vga_buffer;
-mod serial;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    blog_os_println!("Hello world{}", "!"); // Use the renamed import
+    println!("Hello World{}", "!");
+
+    blog_os::init();
+
+    fn stack_overflow() {
+        stack_overflow(); // for each recursion, the return address is pushed
+    }
+
+    // uncomment line below to trigger a stack overflow
+    // stack_overflow();
 
     #[cfg(test)]
     test_main();
+
+    println!("It did not crash!");
     loop {}
 }
 
 /// This function is called on panic.
 #[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    blog_os_println!("{}", _info); // Use the renamed import
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-// our panic handler in test mode
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
 }
 
-#[cfg(test)]
-pub fn test_runner(tests: &[&dyn Fn()]) {
-    serial_println!("Running {} tests", tests.len());
-    for test in tests {
-        test.run();
-    }
-    // new
-    exit_qemu(QemuExitCode::Succes);
-}
-
 #[test_case]
 fn trivial_assertion() {
     assert_eq!(1, 1);
 }
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode {
-    Succes = 0x10,
-    Failed = 0x11,
-}
-
-pub fn exit_qemu(exit_code: QemuExitCode) {
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-pub trait Testable {
-    fn run(&self) -> ();
-}
-
-impl<T> Testable for T
-where
-    T:Fn(),
-    {
-        fn run(&self) {
-            serial_print!("{}...\t", core::any::type_name::<T>());
-            self();
-            serial_println!("[ok]");
-        }
-    }
-
