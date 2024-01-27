508ee8881e0202c077382266fcd953636da9988b
J4spr <jasper07.verbruggen@gmail.com>
2024-01-27 14:10:04 +0100

    implemented heap allocation

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@
 #![test_runner(blog_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
+use alloc::{boxed::Box,rc::Rc};
 use blog_os::println;
 use core::panic::PanicInfo;
 use bootloader::{entry_point, BootInfo};
@@ -14,6 +17,8 @@ entry_point!(kernel_main);
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     use x86_64::{VirtAddr, structures::paging::{Page, Size4KiB}};
     use blog_os::memory::{self, BootInfoFrameAllocator};
+    use blog_os::allocator;
+    use alloc::vec; // Add this line to import the `vec` macro
 
     let _page: Page<Size4KiB> = Page::containing_address(VirtAddr::new(0xdeadbeef000));
 
@@ -25,7 +30,28 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut frame_allocator = unsafe {
         BootInfoFrameAllocator::init(&boot_info.memory_map)
     };
-    
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator)
+        .expect("heap initialisation failed");
+
+    // allocate a number on the heap
+    let heap_value = Box::new(41);
+    println!("heap value at {:p}", heap_value);
+
+    // create a dynamically sized vector
+    let mut vec = vec![]; // Replace `Vec::new()` with `vec![]`
+
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    // create a reference counted vector -> will be freed when count reached 0
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+    println!("current reference count is {}", Rc::strong_count(&cloned_reference));
+    core::mem::drop(reference_counted);
+    println!("reference count is {} now", Rc::strong_count(&cloned_reference));
 
     // map an unused page
     let page = Page::containing_address(VirtAddr::new(0));
